b339b7313aa2ada09dbef875e7c6491d0eb38c96
NIKITA <nik271097@yandex.ru>
2023-03-25 18:28:36 +0300

    5_1

---
+++ Lesson_5/5_1.py
@@ -0,0 +1,14 @@
+# 5.1[26]: Напишите рекурсивную функцию для возведения числа a в степень b. Разрешается использовать только операцию умножения. Циклы использовать нельзя
+# Примеры/Тесты:
+# <function_name>(2,0) -> 1
+# <function_name>(2,1) -> 2
+# <function_name>(2,3) -> 8
+# <function_name>(2,4) -> 16
+def exponentiation(a, b):
+    if b == 0:
+        return 1
+    if b == 1:
+        return a
+    return a*exponentiation(a,b-1)
+
+print(exponentiation(3, 5))
